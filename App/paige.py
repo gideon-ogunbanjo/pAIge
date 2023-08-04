c338cb96419c0209775f4975e3e9607cf6eba202
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-04 20:22:11 +0100

    made minor changes

--- App/paige.py
+++ App/paige.py
@@ -4,15 +4,16 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 # Streamlit app title
-st.title("pAIge - Iris Classification Model")
+st.title("pAIge - Iris Classification Model: ")
 
 # Sidebar for user input
-st.sidebar.header('Input Parameters')
+st.sidebar.header('Input Parameters: ')
+st.sidebar.write("Adjust the sliders to set the values for the parameters of the iris flower.")
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    sepal_length = st.sidebar.slider('Sepal length (Cm)', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width (Cm)', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length (Cm)', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width (Cm)', 0.1, 2.5, 0.2)
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
@@ -22,7 +23,7 @@ def user_input_features():
 
 # Gets user input and display it
 df = user_input_features()
-st.subheader('Inputed parameters')
+st.subheader('Inputed parameters: ')
 st.write(df)
 
 # Load Iris dataset
@@ -35,19 +36,19 @@ clf = RandomForestClassifier()
 clf.fit(X, Y)
 
 # Makes predictions using the user input data
-prediction = clf.predict(df.values)  # Convert DataFrame to NumPy array using .values
-prediction_proba = clf.predict_proba(df.values)  # Convert DataFrame to NumPy array using .values
+prediction = clf.predict(df.values)  # Converts DataFrame to NumPy array using .values
+prediction_proba = clf.predict_proba(df.values)  # Converts DataFrame to NumPy array using .values
 
 # Displays the class labels and their respective index numbers
-st.subheader('Labels of Class and their respective index numbers')
+st.subheader('Labels of Class and their respective index numbers: ')
 st.write(iris.target_names)
 
 # Displays the predicted iris flower species
-st.subheader('Iris Flower Prediction')
+st.subheader('Predicted Iris Flower: ')
 st.write(iris.target_names[prediction])
 
 # Displays the prediction probabilities for each class
-st.subheader('Prediction Probability')
+st.subheader('Prediction Probability: ')
 st.write(prediction_proba)
 
 # Footer with link
